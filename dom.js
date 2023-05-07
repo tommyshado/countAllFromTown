029840d6dcba2badd11a0db261aa51349d77bc32
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-05-07 17:28:00 +0200

    got elements refrence

--- dom.js
+++ dom.js
@@ -1,2 +1,4 @@
 const inputBoxElemRef = document.querySelector('.registrationNumber');
 const dropDownElemRef = document.querySelector('dropdown-item');
+const btnSubmit = document.querySelector('.submitBtn');
+const displayElemRef = document.querySelector('.displayVerifiedReg');
